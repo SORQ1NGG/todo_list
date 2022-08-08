f28e1c7cc98f4815b740385707e9679dfec1cb00
Даниил Ворокосов <daniilvorokosov@mail.ru>
2022-08-08 13:20:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TodoApp } from './components/TodoApp'
+
+function App() {
+  return (
+      <>
+        <h1 style={{ textAlign: 'center' }} >To Do App</h1>
+        <TodoApp />
+      </>
+  )
+}
+
+export default App;
